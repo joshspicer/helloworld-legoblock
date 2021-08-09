d088865b3809d9a3185ef08fff290ae0ad3f22e4
Josh Spicer <hello@joshspicer.com>
2021-08-09 10:57:08 -0400

    sync

--- ubuntu/apply.sh
+++ ubuntu/apply.sh
@@ -2,20 +2,26 @@
 
 tee /usr/hello.sh > /dev/null \
 << 'EOF' 
+RED='\033[0;91m'
+NC='\033[0m' # No Color
+
 while true
+if [[ "$HAS_COLOR" == true ]]; then
+    echo -e "${RED}"
+fi
 do
         case $LANG in
                 "english")
-                        echo -n "Hello, there!"
+                        echo -n -e "Hello, there!"
                         ;;
                 "italian")
-                        echo -n "Salve!"
+                        echo -n -e "Salve!"
                         ;;
                 *)
-                        echo -n "Beep Boop"
+                        echo -n -e "Beep Boop"
         esac
 
-        echo ""
+        echo -e "${NC}"
         sleep 1
 done
 EOF
